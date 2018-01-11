f0ac129d16fdb507dbce857bb262454f7cdb574e
brandonlitwin <brandon.litwin1@marist.edu>
2018-01-11 14:51:47 -0500

    Left numbers actually work properly now

--- picross.js
+++ picross.js
@@ -1,11 +1,11 @@
 var gridId = "";
 //:P face
 var puzzle1 = [
-	[0, 1, 1, 1, 0, 0, 1, 1, 1, 0],
-	[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+	[1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 	[0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
 	[0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-	[0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+	[0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
 	[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
 	[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
 	[0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
@@ -13,28 +13,8 @@ var puzzle1 = [
 	[0, 0, 0, 1, 1, 1, 1, 0, 0, 0]
 ];
 
-var hintNumbers = [
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[],
-	[]
-]; //length 20 but there's 18 hint numbers for puzzle 1
+var leftHintNumbers = [[],[],[],[],[],[],[],[],[],[]]; 
+var topHintNumbers = [[],[],[],[],[],[],[],[],[],[]]; 
 
 function showPuzzleMenu() {
 	document.getElementById("puzzleDropdown").classList.toggle("show");
@@ -56,7 +36,7 @@ function createPuzzle(s, n) {
 	} else if (s == 20) {
 		$('#topNumbers').css("width", "490");
 		$('#leftNumbers').css("height", "490");
-	}	
+	}
 	$("#numbersContainer").show();
 	// Loop that builds a grid in the container
 	for (var rows = 0; rows < s; rows++) {
@@ -66,60 +46,84 @@ function createPuzzle(s, n) {
 			$("#puzzle-container").append("<div id=id class='grid' onclick=squareClicked();></div>");
 			if (columns == 5 || columns == 10 || columns == 15) {
 				$("#id").css("border-left-color", "rgb(231, 170, 101)");
-			} 
+			}
 			if (rows == 5 || rows == 10 || rows == 15) {
 				$("#id").css("border-top-color", "rgb(231, 170, 101)");
 			}
 			$('#id').attr('id', gridId);
-			$('#id').attr('clicked', '0');	
-		}	
+			$('#id').attr('clicked', '0');
+		}
 	}
-		/*Make size of grid*/
-		$(".grid").width(450 / s);
-		$(".grid").height(450 / s);
-	addHintNumbers(s, selectedPuzzle);	
+	/*Make size of grid*/
+	$(".grid").width(450 / s);
+	$(".grid").height(450 / s);
+	addHintNumbers(s, selectedPuzzle);
 }
 
 function addHintNumbers(s, selectedPuzzle) {
-	for(var rows = 0; rows < s; rows++) {
+	//count left numbers
+	for (var rows = 0; rows < s; rows++) {
 		var countLeft = 0;
-		for(var columns = 0; columns < s; columns++) {
-			gridState = selectedPuzzle[rows][columns]; //check state of square in puzzle array
-			var i = 0;
+		for (var columns = 0; columns < s; columns++) {
+			var gridState = selectedPuzzle[rows][columns]; //check state of square in puzzle array
+			console.log("state is " + gridState);
+			console.log("column is " + columns);
+			//var i = 0;
 			if (gridState == 1) {
 				countLeft++; //update count of top nums
 			} else {
+				console.log("this happened");
 				if (countLeft > 0) {
-					hintNumbers[rows].push(countLeft);
+					leftHintNumbers[rows].push(countLeft);
 					//console.log(hintNumbers[rows]);
 					countLeft = 0;
+					//i++;
+				}
+			}
+
+		}
+		console.log("out here");
+		if (countLeft > 0) {
+			leftHintNumbers[rows].push(countLeft);
+			//console.log(hintNumbers[rows]);
+			countLeft = 0;
+			//i++;
+		}
+		//console.log(leftHintNumbers[rows]);
+	}
+	for (var rows = 0; rows < s; rows++) {
+		//Add left numbers
+		$('#leftNumbers').append("<p id=leftid class='hintNumber'></p>");
+		$('#leftid').append(leftHintNumbers[rows]);
+		$('#leftid').attr('id', rows);
+	}
+	//count top numbers
+	/*for (var columns = 0; columns < s; columns++) {
+		var countTop = 0;
+		for (var rows = 0; rows < s; rows++) {
+			var gridState = selectedPuzzle[rows][columns]; //check state of square in puzzle array
+			var i = 0;
+			if (gridState == 1) {
+				countTop++; //update count of top nums
+			} else {
+				if (countTop > 0) {
+					topHintNumbers[rows].push(countTop);
+					//console.log(hintNumbers[rows]);
+					countTop = 0;
 					i++;
 				}
 			}
-			
-			
-
-			/*Add top numbers
-			$('#topNumbers').append("<p id=topid class='hintNumber'></p>");
-			$('#topid').append(countTop);
-			$('#topid').attr('id', rows);*/
 		}
-		console.log(hintNumbers[rows]);
+		console.log(topHintNumbers[columns]);
 	}
-	for(var rows = 0; rows < s; rows++) {
-		//Add left numbers
-		$('#leftNumbers').append("<p id=leftid class='hintNumber'></p>");
-		$('#leftid').append(hintNumbers[rows]);
-		$('#leftid').attr('id', rows);
-		/*for(var columns = 0; columns < s; columns++) {
-		
-			
-			
-		}*/
+	for (var columns = 0; columns < s; columns++) {
+		/*Add top numbers
+		$('#topNumbers').append("<p id=topid class='hintNumber'></p>");
+		$('#topid').append(topHintNumbers[columns]);
+		$('#topid').attr('id', columns);
+	}*/
+
 	
-	}
-	
-	console.log(hintNumbers.length);
 	/*for (var rows = 0; rows < s; rows++) {
 		for (var columns = 0; columns < s; columns++) {
 			newHintNumbers[rows].push(hintNumbers[rows][columns]);
@@ -129,8 +133,8 @@ function addHintNumbers(s, selectedPuzzle) {
 }
 
 function squareClicked() {
-	$(function() {
-		$(".grid").off().on("click", function() {
+	$(function () {
+		$(".grid").off().on("click", function () {
 			$(this).toggleClass("squareClicked");
 		});
 
@@ -140,13 +144,13 @@ function squareClicked() {
 
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
 	if (!event.target.matches('.dropbtn')) {
 
-	var dropdowns = document.getElementsByClassName("dropdown-content");
-	var i;
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
 		for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
+			var openDropdown = dropdowns[i];
 			if (openDropdown.classList.contains('show')) {
 				openDropdown.classList.remove('show');
 			}
